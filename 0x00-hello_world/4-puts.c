1bd2bef65cb0a2cf4bdee96b236d847e1cf93961
Cheruiyot Zephaniah <cheruiyotzeph98@gmail.com>
2023-02-16 12:26:24 -0800

    Print a line with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Main - Program that prints a line with the function puts
+ * Return: 0
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
