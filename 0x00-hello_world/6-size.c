f8a511a837d80b329d7fdbade4cb69a3f5ed5be9
Cheruiyot Zephaniah <cheruiyotzeph98@gmail.com>
2023-02-16 14:30:42 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ * Return: 0
+ */
+int main(void)
+{
+    printf("The size of char is %zu bytes\n", sizeof(char));
+    printf("The size of int is %zu bytes\n", sizeof(int));
+    printf("The size of long int is %zu bytes\n", sizeof(long int));
+    printf("The size of long long int is %zu bytes\n", sizeof(long long int));
+    printf("The size of float is %zu bytes\n", sizeof(float));
+    return 0;
+}
